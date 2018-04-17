729cf907aad1d394bc99053e127ca3637dd7255d
hlebon <hanslgarcia@gmail.com>
2018-04-16 22:44:04 -0500

    feat: new methods to proccess data from server

--- src/components/Map.js
+++ src/components/Map.js
@@ -3,34 +3,58 @@ import ReactMapboxGl, { Layer, Feature } from "react-mapbox-gl";
 import MapFilterOptions from './MapFilterOptions';
 import point from '../Img/map-marker.png';
 import point_blue from '../Img/map-marker-blue.png';
+import { array } from 'lodash'
 
 
 const reportesGenerados = [
-    { lng: -79.70843109554954, lat: 8.869453693785971 },
-    { lng: -79.70752449214012, lat: 8.870167132209687 },
-    { lng: -79.69047536742704, lat: 8.881802047757802 },
-    { lng: -79.6865740592317, lat: 8.883772767074959 },
-    { lng: -79.57353947904005, lat: 8.915508708341903 },
-    { lng: -79.570463227667, lat: 8.91776622493073 },
-    { lng: -79.51502070192475, lat: 8.99336163917377 },
-    { lng: -79.52106857726739, lat: 9.023340757649521 },
-    { lng: -79.54567851200819, lat: 9.047495169669422 },
-    { lng: -79.96463775640501, lat: 9.013563580858474 },
-    { lng: -79.96081643496132, lat: 8.889437919186264 },
-    { lng: -79.95120339791191, lat: 8.657013615441997 },
-    { lng: -79.45939274513825, lat: 9.060541480845103 },
-    { lng: -79.40313764730939, lat: 9.036173632723887 },
-    { lng: -79.65342526042241, lat: 9.00806519121501 },
-    { lng: -79.58520964660124, lat: 9.017807988407583 }
+    { lng: -79.70843109554954, lat: 8.869453693785971, state: "open" },
+    { lng: -79.70752449214012, lat: 8.870167132209687, state: "closed"},
+    { lng: -79.69047536742704, lat: 8.881802047757802, state: "open" },
+    { lng: -79.6865740592317, lat: 8.883772767074959, state: "closed" },
+    { lng: -79.57353947904005, lat: 8.915508708341903,state: "closed" },
+    { lng: -79.570463227667, lat: 8.91776622493073, state: "closed" },
+    { lng: -79.51502070192475, lat: 8.99336163917377, state: "open" },
+    { lng: -79.52106857726739, lat: 9.023340757649521,state: "open" },
+    { lng: -79.54567851200819, lat: 9.047495169669422, state: "open" },
+    { lng: -79.96463775640501, lat: 9.013563580858474, state: "pending" },
+    { lng: -79.96081643496132, lat: 8.889437919186264, state: "pending" },
+    { lng: -79.95120339791191, lat: 8.657013615441997, state: "pending" },
+    { lng: -79.45939274513825, lat: 9.060541480845103, state: "open" },
+    { lng: -79.40313764730939, lat: 9.036173632723887, state: "open" },
+    { lng: -79.65342526042241, lat: 9.00806519121501, state: "open" },
+    { lng: -79.58520964660124, lat: 9.017807988407583, state: "closed" }
 ]
+
+function objectToArray(object){
+	const newElement = []
+	for(let element in object){
+		newElement.push({
+			id: element,
+			data: object[element]
+		})
+	}
+	return newElement;
+}
+
+function groupBy(array){
+    return array.reduce((acc, item)=>{
+        acc[item.state] = acc[item.state] || [];
+        acc[item.state].push(item)
+        return acc
+    }, [])
+}
 
 function generarImg(type) {
     const image = new Image();
-    if(type === "valid"){
+    if(type === "open"){
         image.width = 40;
         image.height = 50;
         image.src = point_blue;
-    }else if(type === "unvalid"){
+    }else if(type === "closed"){
+        image.width = 40;
+        image.height = 50;
+        image.src = point;
+    }else if(type === "pending"){
         image.width = 40;
         image.height = 50;
         image.src = point;
@@ -62,25 +86,10 @@ class Mapbox extends React.Component {
     componentDidMount() {
         const unvalidaData = [];
         const validData = [];
-        const reportes = reportesGenerados.map((obj, id) => {
-            let estado = (((id + 1) % 2) === 0) ? "unvalid" : "valid";
-            if (((id + 1) % 2) === 0) {
-                unvalidaData.push({
-                    id,
-                    enMapa: obj,
-                    estado
-                })
-            } else {
-                validData.push({
-                    id,
-                    enMapa: obj,
-                    estado
-                })
-            }
-        })
-
+        const reportes = objectToArray(groupBy(reportesGenerados));
+        console.log(reportes);
         this.setState({
-            reportes: [unvalidaData, validData]
+            reportes: reportes
         })
     }
 
@@ -113,11 +122,11 @@ class Mapbox extends React.Component {
                                 key={index}
                                 type="symbol"
                                 id={`marker${index}`}
-                                images={[ "huecos"+index, generarImg(newLayer[0].estado)]}
+                                images={[ "huecos"+index, generarImg(newLayer.id)]}
                                 layout={{ "icon-image": "huecos"+index }}>
                                 {
-                                    newLayer.map((reporte, index) => {
-                                        const coordinates = [reporte.enMapa.lng, reporte.enMapa.lat]
+                                    newLayer.data.map((reporte, index) => {
+                                        const coordinates = [reporte.lng, reporte.lat]
                                         return (
                                             <Feature
                                                 key={index}
